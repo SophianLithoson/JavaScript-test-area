d25019760609e5a6480e73858eeb9db5800e5602
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-11-14 20:04:16 -0700

    form-validation exercise, set up form and basic styles

--- js-file.js
+++ js-file.js
@@ -1,12 +1,22 @@
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130,
+const currentLibrary = [];
+
+function Book(title, author, numPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = isRead;
+
+    this.info = function () {
+        return title + " by " + author + ", " + numPages + " pages, " + ((isRead) ? "has been read" : "not read yet");
+    }
 }
 
-function sumSalaries() {
-    let total = 0;
-    for (salary in salaries)
-        total += salaries[salary];
-    return total;
+function addBookToLibrary(tome) {
+    currentLibrary += tome;
 }
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 255, true);
+const soiaf = new Book("Song of Ice and Fire", "G.R.R. Martin", 835, false);
+
+console.log(theHobbit.info());
+console.log(soiaf.info());
